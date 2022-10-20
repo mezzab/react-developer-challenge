d30a8dc5608b9999e60db1d3c21380e45ac7e0b8
Marcos Mezzabotta <marcos32m@gmail.com>
2022-10-19 22:20:26 -0300

    add env files

--- src/config/coinGeckoURLs.tsx
+++ src/config/coinGeckoURLs.tsx
@@ -1,13 +1,12 @@
-const baseUrl = 'https://api.coingecko.com'
-
 export const ListCoins = (
   currency: string,
   perPage: number = 5,
   page: number = 1
 ): string =>
-  `${baseUrl}/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}&sparkline=false`
+  `${process.env.REACT_APP_COIN_GECKO_BASE_URL}/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}&sparkline=false`
 
 export const HistoricalChart = (id: string, currency: string, days: string) =>
-  `${baseUrl}/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`
+  `${process.env.REACT_APP_COIN_GECKO_BASE_URL}/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`
 
-export const SingleCoin = (id: string) => `${baseUrl}/api/v3/coins/${id}`
+export const SingleCoin = (id: string) =>
+  `${process.env.REACT_APP_COIN_GECKO_BASE_URL}/api/v3/coins/${id}`
